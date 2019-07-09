aa7929e41141b5ee6df177677d7474ffe293ef38
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-09 11:55:45 -0300

    First commit

---
+++ start.sh
@@ -0,0 +1,41 @@
+set -euo pipefail
+
+display_info() {
+  printf "Usage ./start.sh [OPT]\nOptions are:\n"
+  printf "  -h: Show this message\n"
+  printf "  -m: Migrate\n"
+  printf "  -s: Seed\n"
+  exit 0
+}
+
+SEED=false
+MIGRATE=false
+while getopts "msh" OPT; do
+  case "$OPT" in
+    "m") MIGRATE=true;;
+    "s") SEED=true;;
+    "h") display_info;;
+    "?") display_info;;
+  esac 
+done
+
+docker network create hermes || true
+
+DOCKER_COMPOSE_OPTS="-p hermes -f docker-compose.dev.yml"
+
+if [ "$MIGRATE" == "true" ] || [ "$SEED" == "true" ]; then
+  docker build -t hermeshub/db-migrator --target=migrator ./db-manager
+  docker-compose $DOCKER_COMPOSE_OPTS up -d db  
+  
+  if [ "$MIGRATE" == "true" ]; then
+    docker run --network=hermes --rm -it hermeshub/db-migrator ./scripts/migrate.sh development
+  fi
+
+  if [ "$SEED" == "true" ]; then
+    docker run --network=hermes --rm -it hermeshub/db-migrator ./scripts/seed.sh development
+  fi
+
+  docker-compose $DOCKER_COMPOSE_OPTS down
+fi
+
+docker-compose $DOCKER_COMPOSE_OPTS up --build
---
+++ stop.sh
@@ -0,0 +1,25 @@
+set -euo pipefail
+
+display_info() {
+  printf "Usage ./stop.sh [OPT]\nOptions are:\n"
+  printf "  -h: Show this message\n"
+  printf "  -v: Delete volumes\n"
+  exit 0
+}
+
+DELETE=false
+while getopts "vh" OPT; do
+  case "$OPT" in
+    "v") DELETE=true;;
+    "h") display_info;;
+    "?") display_info;;
+  esac
+done
+
+DOCKER_COMPOSE_OPTS="-p hermes -f docker-compose.dev.yml"
+
+if [ "$DELETE" == "true" ]; then
+  docker-compose $DOCKER_COMPOSE_OPTS down -v  
+else
+  docker-compose $DOCKER_COMPOSE_OPTS down 
+fi
